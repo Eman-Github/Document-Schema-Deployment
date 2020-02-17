65b86f99bc8b6899dd9af6f475f23a0012588f6d
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-02-17 13:24:39 +0200

    Resolve syntax error 11

--- scripts/prepare-target.sh
+++ scripts/prepare-target.sh
@@ -6,35 +6,39 @@ fi
 par="--name-only"
 echo "par = $par"
 echo "Before Git diff command : git diff $par $1 | sort -u | uniq | grep "docs" "
-CHNAGED_FILE=$(git diff $par $1 | sort -u | uniq | grep "docs")
-echo "CHNAGED_FILE = $CHNAGED_FILE";
+CHANGED_FILE=$(git diff $par $1 | sort -u | uniq | grep "docs")
+echo "CHANGED_FILE = $CHANGED_FILE";
 
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
+if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
    
-   if [[ "$CHNAGED_FILE" == "*docs-test*" ]]; then
+   if [[ "$CHANGED_FILE" == *"docs-test"* ]]; then
      echo "Changes done on docs-test folder";
      export CHANGED_FOLDER="docs-test";
    
-   elif [[ "$CHNAGED_FILE" == "*docs-final*" ]]; then
+   elif [[ "$CHANGED_FILE" == *"docs-final"* ]]; then
      echo "Changes done on docs-final folder";
      export CHANGED_FOLDER="docs-final";
    
-   fi;         
-fi;
+   fi         
+fi
 
-if[[ "$CHNAGED_FILE" == *BillOfLading* ]]; then
+if [[ "$CHANGED_FILE" = *"BillOfLading"* ]]
+then
   echo "BillOfLading document schema has been changed";
   export CHANGED_DOC="BillOfLading";
 
-elif[[ "$CHNAGED_FILE" == *SeaWaybill* ]]; then
+elif [[ "$CHANGED_FILE" = *SeaWaybill* ]]
+then
   echo "SeaWaybill document schema has been changed";
   export CHANGED_DOC="SeaWaybill";
 
-elif[[ "$CHNAGED_FILE" == *VerifyCopy* ]]; then
+elif [[ "$CHANGED_FILE" = *VerifyCopy* ]]
+then
   echo "VerifyCopy document schema has been changed";
   export CHANGED_DOC="VerifyCopy";
 
-elif[[ "$CHNAGED_FILE" == *ShippingInstructions* ]]; then
+elif [[ "$CHANGED_FILE" = *ShippingInstructions* ]]
+then
   echo "ShippingInstructions document schema has been changed";
   export CHANGED_DOC="ShippingInstructions";
 
