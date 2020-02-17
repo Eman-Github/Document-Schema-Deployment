198ddf710f9822f7021d0581d9f96b4f9810b785
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-02-17 09:55:48 +0200

    Update the test folder

--- scripts/prepare-target.sh
+++ scripts/prepare-target.sh
@@ -3,7 +3,14 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
+if [[ -z $2 ]]; then
+    echo "Updated folder name cannot be empty"
+    exit 1
+fi
+
 echo "Before Git diff command"
-git diff --name-only $1 | sort -u | uniq | grep $DEV_TEST_TARGET > /dev/null
-echo "Before Git diff command"
+
+git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
+
+echo "After Git diff command"
 
