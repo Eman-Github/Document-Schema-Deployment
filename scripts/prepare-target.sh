0b80f29a025b520b8682b521642c4ad8ef023d19
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-02-17 09:37:36 +0200

    Add echo commands

--- scripts/prepare-target.sh
+++ scripts/prepare-target.sh
@@ -3,7 +3,7 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
-
+echo "Before Git diff command"
 git diff --name-only $1 | sort -u | uniq | grep $DEV_TEST_TARGET > /dev/null
-
+echo "Before Git diff command"
 
