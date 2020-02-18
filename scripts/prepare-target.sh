767bef33bc398ced7f44b6edaa80b7ad5e2c6a75
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-02-18 18:54:46 +0200

    exit when no file changed

--- scripts/prepare-target.sh
+++ scripts/prepare-target.sh
@@ -14,26 +14,33 @@ echo "CHANGED_FILE is $CHANGED_FILE";
 
 if [[ -z $CHANGED_FILE ]]; then
     echo "No Schema document changed"
+    exit 0
 fi
 
 if [[ "$CHANGED_FILE" = *"BillOfLading"* ]]
 then
   echo "BillOfLading document schema has been changed";
-  export CHANGED_DOC="BillOfLading";
+  export CHANGED_DOC_NAME="Bill Of Lading";
 
 elif [[ "$CHANGED_FILE" = *SeaWaybill* ]]
 then
   echo "SeaWaybill document schema has been changed";
-  export CHANGED_DOC="SeaWaybill";
+  export CHANGED_DOC_NAME="Sea Waybill";
 
 elif [[ "$CHANGED_FILE" = *VerifyCopy* ]]
 then
   echo "VerifyCopy document schema has been changed";
-  export CHANGED_DOC="VerifyCopy";
+  export CHANGED_DOC_NAME="Verify Copy";
 
 elif [[ "$CHANGED_FILE" = *ShippingInstructions* ]]
 then
   echo "ShippingInstructions document schema has been changed";
-  export CHANGED_DOC="ShippingInstructions";
+  export CHANGED_DOC_NAME="Shipping Instructions";
+
+elif [[ "$CHANGED_FILE" = *BookingRequest* ]]
+then
+  echo "ShippingInstructions document schema has been changed";
+  export CHANGED_DOC_NAME="Booking Request";
 
 fi;
+
