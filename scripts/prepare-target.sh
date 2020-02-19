ea08cf708a3b7a3db8da0a08b81cec0593f5153e
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-02-19 11:48:37 +0200

    change where to call git diff

--- scripts/prepare-target.sh
+++ scripts/prepare-target.sh
@@ -10,13 +10,7 @@ par="--name-only"
 echo "par = $par"
 echo "Before Git diff command "
 
-if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(.json$)'
-then
-  echo "No json files are updated, not running the CI."
-  exit
-fi
-
-CHANGED_FILE=$("git diff $par $1 | grep -qvE '(.json$)'")
+CHANGED_FILE=$("git diff $par $1 | sort -u | uniq | grep "docs-schema"")
 
 echo "CHANGED_FILE is $CHANGED_FILE";
 
