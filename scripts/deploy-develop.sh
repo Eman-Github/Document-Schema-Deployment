4d089a520c28c562042e5280c6c7b7855d4caf8e
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-03-04 16:52:43 +0200

    Reading from csv file 8

--- scripts/deploy-develop.sh
+++ scripts/deploy-develop.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -ev
 
-# - CHANGED_DOC_NAME: Check if the CHANGED_FOLDER is test or final
+if [ -z $1 ]; then
+    echo "CHANGED_FILE can't be null ";
+    exit;
+   fi;
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
    echo $TRAVIS_BRANCH;
-   if [ -z $1 ]; then
-    echo "CHANGED_FILE can't be null ";
-#    exit;
-   fi;
-   if [ -z "$CHANGED_DOC_NAME" ]; then
-    echo "CHANGED_DOC_NAME can't be null ";
-#    exit;
-   fi;
-   
    export BRANCH=$TRAVIS_BRANCH
    export TRIGGERED_BY="PUSH"
 else
@@ -27,7 +22,7 @@ HEADER_CONTENT_TYPE="Content-Type:application/x-www-form-urlencoded"
 BODY="grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=$DEV_API_KEY"
 
 #echo "parameters = $HEADER_CONTENT_TYPE and $BODY "
-RESPONSE_REFRESH_TOKEN=`curl --location --request POST 'https://iam.ng.bluemix.net/oidc/token' --header ${HEADER_CONTENT_TYPE} --data-raw ${BODY}`
+#RESPONSE_REFRESH_TOKEN=`curl --location --request POST 'https://iam.ng.bluemix.net/oidc/token' --header ${HEADER_CONTENT_TYPE} --data-raw ${BODY}`
 
 #---------------------------------------------------------------------------------
 
@@ -38,14 +33,14 @@ HEADER_ACCEPT="Accept:application/json"
 URL="$DEV_URL/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority"
 #echo "URL = $URL"
 
-RESPONSE_BEARER=`curl --location --request POST 'https://platform-dev.tradelens.com/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority' \
---header ${HEADER_CONTENT_TYPE} \
---header ${HEADER_ACCEPT} \
---data-raw "${RESPONSE_REFRESH_TOKEN}"`
+#RESPONSE_BEARER=`curl --location --request POST 'https://platform-dev.tradelens.com/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority' \
+#--header ${HEADER_CONTENT_TYPE} \
+#--header ${HEADER_ACCEPT} \
+#--data-raw "${RESPONSE_REFRESH_TOKEN}"`
 
 #echo "RESPONSE_BEARER = $RESPONSE_BEARER"
 
-BEARER_TOKEN=`echo $RESPONSE_BEARER | grep -oP '(?<="onboarding_token":")[^"]*'`
+#BEARER_TOKEN=`echo $RESPONSE_BEARER | grep -oP '(?<="onboarding_token":")[^"]*'`
 
 #------------------------------------------------------------------------------------
 #Get the Document Schema Id from document_schema_data.csv file
@@ -76,9 +71,9 @@ DEV_API_URL="$DEV_URL/api/v1/documentSchema/${data[3]}"
 
 echo "DEV_API_URL = $DEV_API_URL"
  
-RESPONSE=`curl --location --request GET "$DEV_API_URL" \
---header "${HEADER_AUTHORIZATION}"`
-echo "RESPONSE = $RESPONSE"
+#RESPONSE=`curl --location --request GET "$DEV_API_URL" \
+#--header "${HEADER_AUTHORIZATION}"`
+#echo "RESPONSE = $RESPONSE"
 
 JSON_FILE=`cat "${1}"`
 echo "$JSON_FILE"
@@ -88,6 +83,6 @@ echo "$JSON_FILE"
 #--data-raw "${JSON_FILE}"`
 
 
-echo "curl --location --request PUT "$DEV_API_URL" --header "${HEADER_AUTHORIZATION}" --data-raw "${JSON_FILE}" "
+#echo "curl --location --request PUT "$DEV_API_URL" --header "${HEADER_AUTHORIZATION}" --data-raw "${JSON_FILE}" "
 #-----------------------------------------------------------------------------------
 
