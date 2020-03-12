8b8cf396e6b1c12f41f4e5de05a79f1a41fcdc7e
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-03-12 09:54:14 +0200

    resolve syntax error 3

--- scripts/deploy-develop.sh
+++ scripts/deploy-develop.sh
@@ -22,8 +22,8 @@ HEADER_CONTENT_TYPE="Content-Type:application/x-www-form-urlencoded"
 BODY="grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=$DEV_API_KEY"
 
 #echo "parameters = $HEADER_CONTENT_TYPE and $BODY "
-#RESPONSE_REFRESH_TOKEN=`curl --location --request POST 'https://iam.ng.bluemix.net/oidc/token' --header ${HEADER_CONTENT_TYPE} --data-raw ${BODY}`
-
+RESPONSE_REFRESH_TOKEN=`curl --location --request POST 'https://iam.ng.bluemix.net/oidc/token' --header ${HEADER_CONTENT_TYPE} --data-raw ${BODY}`
+echo "RESPONSE_REFRESH_TOKEN = $RESPONSE_REFRESH_TOKEN"
 #---------------------------------------------------------------------------------
 
 #Getting Bearer Token
@@ -33,15 +33,15 @@ HEADER_ACCEPT="Accept:application/json"
 URL="$DEV_URL/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority"
 #echo "URL = $URL"
 
-#RESPONSE_BEARER=`curl --location --request POST 'https://platform-dev.tradelens.com/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority' \
-#--header ${HEADER_CONTENT_TYPE} \
-#--header ${HEADER_ACCEPT} \
-#--data-raw "${RESPONSE_REFRESH_TOKEN}"`
+RESPONSE_BEARER=`curl --location --request POST 'https://platform-dev.tradelens.com/onboarding/v1/iam/exchange_token/solution/gtd-dev/organization/gtd-ibm-authority' \
+--header ${HEADER_CONTENT_TYPE} \
+--header ${HEADER_ACCEPT} \
+--data-raw "${RESPONSE_REFRESH_TOKEN}"`
 
 #echo "RESPONSE_BEARER = $RESPONSE_BEARER"
 
-#BEARER_TOKEN=`echo $RESPONSE_BEARER | grep -oP '(?<="onboarding_token":")[^"]*'`
-
+BEARER_TOKEN=`echo $RESPONSE_BEARER | grep -oP '(?<="onboarding_token":")[^"]*'`
+echo "BEARER_TOKEN = $BEARER_TOKEN"
 #------------------------------------------------------------------------------------
 #Get the Document Schema Id from document_schema_data.csv file
 #==============================================================
@@ -62,8 +62,8 @@ do
 done
 
 #-----------------------------------------------------------------------------------
-#Getting Bearer Token
-#==============================
+#Call API to deploy the Document Schema 
+#=========================================
 HEADER_CONTENT_TYPE="Content-Type:application/json"
 HEADER_ACCEPT="Accept:application/json"
 HEADER_AUTHORIZATION="Authorization: Bearer $BEARER_TOKEN"
@@ -71,9 +71,9 @@ DEV_API_URL="$DEV_URL/api/v1/documentSchema/${data[3]}"
 
 echo "DEV_API_URL = $DEV_API_URL"
  
-#RESPONSE=`curl --location --request GET "$DEV_API_URL" \
-#--header "${HEADER_AUTHORIZATION}"`
-#echo "RESPONSE = $RESPONSE"
+RESPONSE=`curl --location --request GET "$DEV_API_URL" \
+--header "${HEADER_AUTHORIZATION}"`
+echo "RESPONSE = $RESPONSE"
 
 JSON_FILE=`cat "${1}"`
 echo "$JSON_FILE"
