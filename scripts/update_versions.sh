f0c0197453640d167ef9806241cefd9d81e29be4
Eman Abdel Atty <emanaty@eg.ibm.com>
2020-03-24 17:48:28 +0200

    Try to tag commit 3

--- scripts/update_versions.sh
+++ scripts/update_versions.sh
@@ -33,7 +33,7 @@ for i in "${!data[@]}"
 do
    echo "$i ${data[i]}"
    if (($i == 5)) ; then   
-      TAG_VERSION="data[i]."   
+      TAG_VERSION="${data[i]}."   
    fi;
    if (($i == 6)) ; then
  
@@ -41,14 +41,14 @@ do
        ((data[i]=data[i]+1));
        echo "$i after increment ${data[i]}";
      fi;
-     TAG_VERSION="$TAG_VERSIONdata[i]."
+     TAG_VERSION="$TAG_VERSION${data[i]}."
 
    elif (($i == 7)); then
      if [[ "$FROM_BRANCH" == *"fixbug"* ]]; then
         ((data[i]=data[i]+1));
         echo "$i after increment ${data[i]}";
      fi;
-     TAG_VERSION="$TAG_VERSIONdata[i]"
+     TAG_VERSION="$TAG_VERSION${data[i]}"
 
    fi;
 
